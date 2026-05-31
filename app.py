ef160f6b45905959c35dbb8813181e3dcb99d1ef
Strahil Peykov <strahil.peykov@teampicnic.com>
2026-05-31 17:49:06 +0200

    security: fix CORS wildcard headers with credentials

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ app.add_middleware(
     allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Authorization", "X-Requested-With", "X-Tz-Offset", "X-Odysseus-Owner", "X-Odysseus-Internal-Token"],
 )
 
 # ========= SECURITY HEADERS MIDDLEWARE =========
