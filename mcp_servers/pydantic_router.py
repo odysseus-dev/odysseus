5d1db106cb4205e2ecd88da7c839142e0eee236b
vishywashy <vishrutgiridhar@gmail.com>
2026-06-11 14:04:57 +0100

    refactor(mcp): convert hardcoded routing variables to dynamic arguments

--- mcp_servers/pydantic_router.py
+++ mcp_servers/pydantic_router.py
@@ -8,28 +8,16 @@ class AgentRouter(BaseModel):#This is the way that the AI formats its response s
 
 
     
-async def Route(prompt):
-    ODYSSEUS_SERVERS = {            #Defined dictionary for key value pairs so AI response is mapped out to the correct file
-    "rag":"rag_server.py",
-    "image":"image_gen_server.py",
-    "email":"email_server.py"
-    }
+async def Route(prompt, Agent_Hashmap:dict, system_prompt, model):
     response = await asyncio.to_thread(ollama.generate,                    #ollama.generate is synchronous so I created a new thread to carry out the execution.
-    model='llama3.2',
+    model=model,
     prompt= prompt,
     format=AgentRouter.model_json_schema(),
-    system= """Choose between three agents. 
-     **Email agent responsible for sending an email.**
-     **Image agent responsible for generating images.**
-     ** rag agent for dealing with any external documents.**
-     Make sure you say the name of the agent but don't add the word agent after it. For example, Email, Image, Rag respectively.
-
-
-     """ 
+    system=system_prompt
      )
     Output = AgentRouter.model_validate_json(response['response']).Agent.lower()
     print(Output)
-    IdentifyingAgent = ODYSSEUS_SERVERS.get(Output)#Maps it out with dictionary to look at what mcp agent should be called
+    IdentifyingAgent = Agent_Hashmap.get(Output)#Maps it out with dictionary to look at what mcp agent should be called
     if not IdentifyingAgent:
         return
     transport = StdioTransport("python", args=[f"mcp_servers/{IdentifyingAgent}"])
